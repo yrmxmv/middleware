18eb7fa7b6ee952db40207211968ffeba6560175
yura <yura@abovebits.com>
2021-01-06 01:46:57 +0200

    added db to ignore

--- etc/params.php
+++ etc/params.php
@@ -4,16 +4,3 @@ return [
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
 ];
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock;dbname=middleware',
-    'username' => 'middleware',
-    'password' => 'middleware',
-    'charset' => 'utf8mb4',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
-];
